cd022e5689a778123dd96ab114c81b446815a6bc
Boren Zhang <bobo1993324@gmail.com>
2014-03-15 18:05:03 -0400

    add package.sh

---
+++ package.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+mkdir package/plugins/UReader -p
+cp components ui uReader.qml manifest.json uReader.desktop img uReader.json ./package -rf
+cp plugins/UReader/libfilesplugin.so plugins/UReader/qmldir package/plugins/UReader/
+cd package
+click build .
+cp *.click ..
+cd ..
+#rm -rf package
+
--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@ cd plugins
 qmake
 make
 cd ..
-qmlscene -I plugins uReader.qml
+qmlscene -I plugins uReader.qml --desktop_file_hint=`pwd`/uReader.desktop
